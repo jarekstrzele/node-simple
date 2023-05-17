a15116e3dcf7665dffdedbe8ef5d8e3d6555f0d2
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-17 17:30:58 +0200

    Addm Joi library

--- node_mosh/express-demo/index.js
+++ node_mosh/express-demo/index.js
@@ -1,3 +1,5 @@
+const Joi = require("joi") ; // write `Joi` because require returns a class
+
 const express = require('express') ;
 
 // by convention use the name 'app'
@@ -7,7 +9,7 @@ app.use(express.json()) ;
 const courses = [
 	{id: 1, name: 'course1'} , 
 	{id: 2, name: 'course2'} ,
-	{id: 3, name: 'course3'} ,
+	{id: 3, name: 'course3'} ,  
 ];
 
 app.get('/', (req, res) => {
@@ -28,9 +30,50 @@ app.get('/api/courses/:id', (req, res) => {
 }) ;
 
 app.post('/api/courses', (req, res) => {
+	
+	const schema = Joi.object( {
+		name: Joi.string().min(3).required()
+	}) ;
+	
+	const result = schema.validate(req.body)
+	console.log(result) ;
+
+	if (result.error){
+		//res.status(400).send(result.error) ;
+		res.status(400).send(result.error.details[0].message) ;
+		return ;
+	}
+
+// update course:
+app.put('/api/courses/:id', (req, res) => {
+	//look up the courses
+	// if not existing, return 404
+	const course = courses.find(c => c.id === parseInt(req.params.id)) ;
+	if (!course) res.status(404).send('the course with the given id was not found') ;
+
+	// validate
+	//if invalid, return 400
+	const schema = Joi.object( {
+		name: Joi.string().min(3).required()
+	}) ;
+	
+	const result = schema.validate(req.body)
+	console.log(result) ;
+
+	if (result.error){
+		//res.status(400).send(result.error) ;
+		res.status(400).send(result.error.details[0].message) ;
+		return ;
+	}
+
+	//update course
+	//return the updated course
+	course.name = req.body.name ;
+	res.send(course) ;
+})
 	const course = {
 		id: courses.length + 1,
-		name: req.body.name
+		name: req.body.name //we assume that in the body is an object with 'name' property 
 	}
 	courses.push(course) ;
 	res.send(course) ;
