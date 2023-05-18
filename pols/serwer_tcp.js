b36731c0edebac9614bd4b2fc89d9f75692d7b0c
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-18 14:19:58 +0200

    Add a simple http server

--- pols/serwer_tcp.js
+++ pols/serwer_tcp.js
@@ -1,16 +1,18 @@
-const fs = require("fs") ;
-const net = require("net") ; //moduł bazuje na protokole TCP/IP
+const http = require("http") ;
 
-// socket - to podłączony klient
-const server = net.createServer((socket) => {
-    //socket to stream
-    socket.write("hello world") ;
 
-    socket.end("Koniec połączenia") ;
+const serwer = http.createServer( (req,res) => {
+    res.write("Hello from http") ;
+    res.end() ;
 
 })
 
-server.listen(8080, ()=> console.log("Serwer uruchomiony 127.0.0.1:8080"));
+
+
+serwer.listen(8000, ()=> {
+    console.log("Serwer uruchomiony 127.0.0.1:8000");
+}
+)
 
 
 
