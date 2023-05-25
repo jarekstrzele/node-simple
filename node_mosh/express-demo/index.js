8ff7e2c02a8b5ea254622b1207af47f21ea9c6f3
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-25 12:03:15 +0200

    Add validation function

--- node_mosh/express-demo/index.js
+++ node_mosh/express-demo/index.js
@@ -31,18 +31,28 @@ app.get('/api/courses/:id', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
 	
-	const schema = Joi.object( {
-		name: Joi.string().min(3).required()
-	}) ;
+	// const schema = Joi.object( {
+	// 	name: Joi.string().min(3).required()
+	// }) ;
 	
-	const result = schema.validate(req.body)
-	console.log(result) ;
-
-	if (result.error){
+	// const result = schema.validate(req.body)
+	// //console.log(result) ;
+	const { error } = validateCourse(req.body) ;
+	if (error){
 		//res.status(400).send(result.error) ;
-		res.status(400).send(result.error.details[0].message) ;
+		res.status(400).send(error.details[0].message) ;
 		return ;
 	}
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name
+	};
+
+	courses.push(course) ;
+	res.send(course) ;
+
+
+}) ;
 
 // update course:
 app.put('/api/courses/:id', (req, res) => {
@@ -53,16 +63,17 @@ app.put('/api/courses/:id', (req, res) => {
 
 	// validate
 	//if invalid, return 400
-	const schema = Joi.object( {
-		name: Joi.string().min(3).required()
-	}) ;
-	
-	const result = schema.validate(req.body)
-	console.log(result) ;
-
-	if (result.error){
+	// const schema = Joi.object( {
+	// 	name: Joi.string().min(3).required()
+	// }) ;
+	// const result = schema.validate(req.body)
+	// console.log(result) ;
+	//instead of this code above :
+	//const result = validateCourse(req.body)
+	const { error } = validateCourse(req.body)
+	if (error){
 		//res.status(400).send(result.error) ;
-		res.status(400).send(result.error.details[0].message) ;
+		res.status(400).send(error.details[0].message) ;
 		return ;
 	}
 
@@ -70,15 +81,17 @@ app.put('/api/courses/:id', (req, res) => {
 	//return the updated course
 	course.name = req.body.name ;
 	res.send(course) ;
-})
-	const course = {
-		id: courses.length + 1,
-		name: req.body.name //we assume that in the body is an object with 'name' property 
-	}
-	courses.push(course) ;
-	res.send(course) ;
-	
 }) ;
+	
+
+function validateCourse(course){
+	const schema = Joi.object( {
+		name: Joi.string().min(3).required()
+	}) ;
+	
+	return schema.validate(course) ;
+	
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on the port ${port}...`)) ;
